ae5773ed6f56b0b68e9ef8e359d7b122334be7d8
Peter Le <Peterle3@csus.edu>
2016-12-03 16:43:45 -0800

    test commit

--- main.js
+++ main.js
@@ -1,45 +1,32 @@
 var canvas = document.querySelector('#canvas');
+var canvas2 = document.getElementById('healthBar');
 var context = canvas.getContext('2d');
+var context2 = canvas2.getContext('2d');
+
+/* Canvas height/width */
+cHeight = 500;
+cWidth = 700;
+
+
+/* Player's initial position (bottom center) */
+var xPos = 325;
+var yPos = 450;
+
+var score = 0;
+var gameOver = 0; //-1 if game is over.
 
 
 
-// hello worlds
-var HEIGHT = 50;
-var WIDTH = 50;
-var xPos = 0;
-var yPos = 0;
-var hp = 0;
+/* Scoreboard */
+context2.rect(0, 0, 700, 70); //Draw the top canvas (score)
+context2.stroke();
 
-// iterate through enemys in enemylist, check if enemy and player collide
+/*Main game screen */
+context.rect(xPos, yPos, 50, 50); //Draw the second canvas (main game)
+context.stroke();
 
-// gets distance between 2 objects
-getDistanceBetweenObject = function (object1, object2) { 
-
-
-}
-
-var enemy = { };
-//var upgradeList= { };
-/**
-creates RandomEnemy that object has to dodge
-@param x is x coordinate of enemy
-@param y is y coordinate of enemy
-@param width is
-@ param height is
-@ param id is an object number
-*/
-
-	// creates rectangle to move around
-	
-	context.rect(xPos, yPos, 50, 50);
-	context.stroke();
-	context.beginPath();
-    context.moveTo(0, 100);
-    context.lineTo(canvas.width,100);
-    context.stroke();
-    
-   // enemy is a class constructor
-	var enemy = function(id, x, y, width, height){
+/* Randomly generate enemy */
+var enemy = function(id, x, y, width, height){
 		this.id = id;
 		this.x = x;
 		this.y = y;
@@ -50,47 +37,21 @@ creates RandomEnemy that object has to dodge
 	//enemyList[id] = enemy2;
 	//generateRandomEnemy is a class generator since I'm returning an instance of enemy
 	var generateRandomEnemy = function() { 
-		var x = Math.random() * WIDTH;
-		var y = Math.random() * HEIGHT;
+		var x = Math.random() * cWidth;
+		var y = Math.random() * cHeight;
 		var height = 10 + Math.random() * 30;
 		var width = Math.random() * 30;
 		var id = Math.random();
 		return new enemy(id, x, y, width, height);
-	
-	
 	}
 	var enemy = generateRandomEnemy();
 	
 	console.log(enemy);
 	context.rect(enemy.x, enemy.y, enemy.width, enemy.height);
 	context.stroke();
-	
 
 
-/*Upgrade = function(id, x,y, width, height) { 
-	var enemy = { 
-		x:x,
-		y:y,
-		id:id,
-		width:width,
-		
-	
-	}
-	upgradeList[id] = enemy;
 
-	
-}
-	generateRandomUpgrade = function(){
-        //Math.random() returns a number between 0 and 1
-        var x = Math.random()*WIDTH;
-        var y = Math.random()*HEIGHT;
-        var height = 10;
-        var width = 10;
-        var id = Math.random();
-        
-        Upgrade(id,x,y,width,height);
-}
-*/
 
 
 function move(e){
@@ -98,7 +59,7 @@ function move(e){
 	//alert(e.keyCode);
 
 	if(e.keyCode == 39){ //Right arrow key
-		// if less than boundary on right side, move right, if not do nothing
+		//console.log(`xPos: ${xPos}\n`);
 		if (xPos < 650) xPos +=5; 
 	}
 	else if(e.keyCode == 37) { //Left arrow key
@@ -106,26 +67,46 @@ function move(e){
 	}
 
 	if(e.keyCode == 38) { //Up arrow key
-		if (yPos > 100) yPos -= 5;
+		//if (yPos > 100) yPos -= 5;
+		if (yPos > 0) yPos -= 5;
 	}
 	else if(e.keyCode == 40) { //Down arrow key
 		if (yPos < 450) yPos +=5;
 	}
-	    canvas.width = canvas.width;
-	    context.rect(xPos, yPos, 50, 50);
-	    context.stroke();
-	    
 	
-	
-	
-
-
-	// Peter upper boundary line
-	context.beginPath();
-    context.moveTo(0, 100);
-    context.lineTo(canvas.width,100);
-    context.stroke();
+	canvas.width = canvas.width;
+	context.rect(xPos, yPos, 50, 50);
+	context.stroke();
 }
+
+//Score Counter
+function myFunction() {
+	if (gameOver == 0) setInterval(alertFunc, 500);
+}
+
+function alertFunc() {
+    score++;
+}
+
+setInterval(function(){
+	if (gameOver == 0)
+	{
+		canvas2.width = canvas2.width;
+		context2.font="20px Arial";
+		context2.fillText(`Score: ${score}`, 5 ,50)
+		context2.rect(0, 0, 700, 70);
+		context2.stroke();
+	}
+	else {
+		canvas2.width = canvas2.width;
+		context2.font="20px Arial";
+		context2.fillText(`GAME OVER    Score: ${score}`, 5 ,50)
+		context2.rect(0, 0, 700, 70);
+		context2.stroke();
+	}
+}, 100);
+
+
 
 
 document.onkeydown = move;
