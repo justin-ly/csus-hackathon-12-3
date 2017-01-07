eeb26beb4536f550ff020a7b1279874bfb1e2957
Peter Le <Peterle3@csus.edu>
2017-01-07 11:59:31 -0800

    Fixed Boundaries and CSS

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-var canvas = document.querySelector('#canvas');
-var canvas2 = document.querySelector('#score');
+var canvas = document.querySelector('.bottom');
+var canvas2 = document.querySelector('.top');
 //var canvas3 = document.querySelector('enemy');
 
 var context = canvas.getContext('2d');
@@ -10,8 +10,8 @@ enemyCount = 10;
 health = 3;
 
 /* Canvas height/width */
-cHeight = 500;
-cWidth = 700;
+cHeight = 450;
+cWidth = 650;
 
 
 
@@ -64,7 +64,8 @@ var enemy = function( x, y, width, height){
 		var x = Math.random() * cWidth;
 		var y = Math.random() * cHeight;
 		var height = 10 + Math.random() * 30;
-		var width = Math.random() * 30;
+		var width = 10 + Math.random() * 30;
+
 		
 		return new enemy(x, y, width, height);
 	}
@@ -79,8 +80,9 @@ var enemy = function( x, y, width, height){
 	}
 	
 	
-	
+	context.strokeStyle = "red";
 	context.rect(enemy.x, enemy.y, enemy.width, enemy.height);
+
 	context.stroke();
 	
 	
@@ -89,30 +91,30 @@ var enemy = function( x, y, width, height){
 	// clears the canvas and won't leave the trail for the enemy
 	canvas.width = canvas.width;
 	
+	//Boundary check
 	for(var i = 0; i < enemyCount; i++) {
-	if(enemyList[i].x >650) { 
-		enemyList[i].xDir= -enemyList[i].xDir;
-	}
-	if(enemyList[i].x< 0) { 
-		enemyList[i].xDir= -enemyList[i].xDir;
-	}
-	if(enemyList[i].y >450) { 
-		enemyList[i].yDir= -enemyList[i].yDir;
-	}
-	if(enemyList[i].y <0) { 
-		enemyList[i].yDir= -enemyList[i].yDir;
-	}
-	enemyList[i].x += enemyList[i].xDir;
-	enemyList[i].y += enemyList[i].yDir;
-
+		if(enemyList[i].x + enemyList[i].width > 700) { 
+			enemyList[i].xDir= -enemyList[i].xDir;
+		}
+		if(enemyList[i].x < 0) { 
+			enemyList[i].xDir= -enemyList[i].xDir;
+		}
+		if(enemyList[i].y + enemyList[i].height > 500) { 
+			enemyList[i].yDir= -enemyList[i].yDir;
+		}
+		if(enemyList[i].y < 0) { 
+			enemyList[i].yDir= -enemyList[i].yDir;
+		}
+		enemyList[i].x += enemyList[i].xDir;
+		enemyList[i].y += enemyList[i].yDir;
+	 
 	context.rect(enemyList[i].x, enemyList[i].y, enemyList[i].width, enemyList[i].height);
 	context.stroke();
-	}
+	} //end for
 	context.rect(player.x, player.y, 50, 50);
-	context.stroke();
+	context.stroke();	
+}, 50)
 
-	
-	},50)
 
 
 
@@ -123,18 +125,18 @@ function move(e){
 	//alert(e.keyCode);
 
 	if(e.keyCode == 39){ //Right arrow key
-		if (player.x < 650) player.x +=10; 
+		if (player.x < 650) player.x +=30; 
 	}
 	else if(e.keyCode == 37) { //Left arrow key
-		if (player.x > 0) player.x -=10;
+		if (player.x > 0) player.x -=30;
 	}
 
 	if(e.keyCode == 38) { //Up arrow key
 		//if (yPos > 100) yPos -= 5;
-		if (player.y > 0) player.y -= 10;
+		if (player.y > 0) player.y -= 30;
 	}
 	else if(e.keyCode == 40) { //Down arrow key
-		if (player.y < 450) player.y +=10;
+		if (player.y < 450) player.y +=30;
 	}
 	
 	/*canvas.width = canvas.width;
