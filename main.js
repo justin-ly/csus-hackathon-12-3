f390dc34161f90c0ceec6e5f6c8f69f2390822de
Peter Le <Peterle3@csus.edu>
2017-01-15 19:20:13 -0800

    Collision Check and Colors

--- main.js
+++ main.js
@@ -6,13 +6,12 @@ var context = canvas.getContext('2d');
 var context2 = canvas2.getContext('2d');
 
 // amount of enemies
-enemyCount = 10;
-health = 3;
+var enemyCount = 5;
+var health = 3;
 
-/* Canvas height/width */
-cHeight = 450;
-cWidth = 650;
-
+/* Max starting location of enemy is (450, 650) */
+var enemYPos = 450;
+var enemXPos = 650;
 
 
 /* Player's initial position (bottom center) */
@@ -22,28 +21,20 @@ var yPos = 450;
 var score = 0;
 var gameOver = 0; //-1 if game is over.
 
+var pause = false;
 
-
-/* Scoreboard */
-context2.rect(0, 0, 700, 70); //Draw the top canvas (score)
-context2.stroke();
-
-/*Main game screen */
-context.rect(xPos, yPos, 50, 50); //Draw the second canvas (main game)
-context.stroke();
 
 // player class 
-var player = function( x, y, width, height){
-		
+	var player = function( x, y, width, height){
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;	
 	}
-	/*
+	var player = new player(325,450,50,50);
 
 /* enemy is a constructor */
-var enemy = function( x, y, width, height){
+	var enemy = function( x, y, width, height){
 		this.x = x;
 		this.y = y;
 		this.width = width;
@@ -56,41 +47,50 @@ var enemy = function( x, y, width, height){
 	  if(Math.random() <=.5) { 
 			this.yDir = -this.yDir;
 	  }
+
 	}
 	
-	//enemyList[id] = enemy2;
 	//generateRandomEnemy is a class generator since I'm returning an instance of enemy
 	var generateRandomEnemy = function() { 
-		var x = Math.random() * cWidth;
-		var y = Math.random() * cHeight;
-		var height = 10 + Math.random() * 30;
-		var width = 10 + Math.random() * 30;
+		var x = Math.random() * enemXPos;
+		var y = Math.random() * enemYPos;
+		var height = 15 + Math.random() * 30;
+		var width = 15 + Math.random() * 30;
+	
 
 		
 		return new enemy(x, y, width, height);
 	}
-	var player = new player(325,450,25,25)
+	//array of enemies
 	var enemyList = [];
-	
+
 	for(var i = 0; i < enemyCount; i++) { 
 		//to put elements inside array, use push
 		// to remove elements, use pop
 		enemyList.push(generateRandomEnemy());
 		
 	}
+	// this will generate a new enemey every 3 seconds
+	var moreEnemy = setInterval(function() {
 	
+	enemyList.push(generateRandomEnemy());
+	enemyCount++;
+	//whenever certain enemyCount is at desired limit,stop this function from creating more enemies
+	if(enemyCount == 10) {
+		clearInterval(moreEnemy);
+	}
 	
-	context.strokeStyle = "red";
-	context.rect(enemy.x, enemy.y, enemy.width, enemy.height);
+	}, 3000)
+	
+	//context.rect(enemy.x, enemy.y, enemy.width, enemy.height);
 
-	context.stroke();
+	//context.stroke();
 	
 	
 	
-	setInterval(function() {
+	var stop =setInterval(function() {
 	// clears the canvas and won't leave the trail for the enemy
 	canvas.width = canvas.width;
-	
 	//Boundary check
 	for(var i = 0; i < enemyCount; i++) {
 		if(enemyList[i].x + enemyList[i].width > 700) { 
@@ -107,25 +107,76 @@ var enemy = function( x, y, width, height){
 		}
 		enemyList[i].x += enemyList[i].xDir;
 		enemyList[i].y += enemyList[i].yDir;
-	 
-	context.rect(enemyList[i].x, enemyList[i].y, enemyList[i].width, enemyList[i].height);
-	context.stroke();
+     	context.fillStyle = "#FF0000";
+	    context.fillRect(enemyList[i].x, enemyList[i].y, enemyList[i].width, enemyList[i].height);
+	    context.stroke();
 	} //end for
-	context.rect(player.x, player.y, 50, 50);
+
+	//fills color and draws the player rectange
+	context.fillStyle = "#00ace6";
+	context.fillRect(player.x, player.y, player.width, player.height);
 	context.stroke();	
+	//checks
+	// checks for collisions between player and enemy here
+//	collisionCheck();
+
+
 }, 50)
 
+//This is the collision check
+ setInterval(function() {
+ 	for(var i = 0; i < enemyCount; i++) {
+ 		var check = true;
+		// checks if player is within enemy.x +width AND enemy.y + height
+		if(player.x < enemyList[i].x + enemyList[i].width &&
+			player.x + player.width > enemyList[i].x &&
+			player.y < enemyList[i].y + enemyList[i].height &&
+			player.y + player.height > enemyList[i].y)  {
+			health--;	
+			check = false;
 
+		}
 
+		if(health == 0) {
+			gameOver++;
+			clearInterval(stop);
+			clearInterval(scoreCount);
+		}
 
+	}
+	
+	
+	}, 300)
+/*function collisionCheck(){
+	for(var i = 0; i < enemyCount; i++) {
+	
+		}
+		/* Why doesn't this work???
+		if(player.x == enemyList[i].x + enemyList[i].width ||
+			player.x + player.width == enemyList[i].x ||
+			player.y == enemyList[i].y + enemyList[i].height ||
+			player.y + player.height == enemyList[i].y) {
+				health--;
+		}
+		
 
+		if(health == 0) {
+			gameOver++;
+			clearInterval(stop);
+			clearInterval(scoreCount);
+		}
 
+	}
+}
+*/
+
+	
 function move(e){
 
 	//alert(e.keyCode);
 
 	if(e.keyCode == 39){ //Right arrow key
-		if (player.x < 650) player.x +=30; 
+		if (player.x + player.width < 700) player.x +=30; 
 	}
 	else if(e.keyCode == 37) { //Left arrow key
 		if (player.x > 0) player.x -=30;
@@ -136,23 +187,41 @@ function move(e){
 		if (player.y > 0) player.y -= 30;
 	}
 	else if(e.keyCode == 40) { //Down arrow key
-		if (player.y < 450) player.y +=30;
+		if (player.y  + player.height< 500) player.y +=30;
+	}
+	else if(e.keyCode == 80) {
+	
+		!pause;
+		pauseGame();
+
+
+
+	}
+
+
+	function pauseGame() {
+		if(!pause) {
+			clearInterval(stop);
+			clearInterval(scoreCount);
+		 document.getElementById('test').style.display = "block";
+		
+		
+			
+		}
+	
+	
+
+
 	}
 	
-	/*canvas.width = canvas.width;
-	context.rect(xPos, yPos, 50, 50);
-	context.stroke();
-	*/
 }
 
 //Score Counter
-function myFunction() {
-	if (gameOver == 0) setInterval(alertFunc, 500);
-}
 
-function alertFunc() {
-    score++;
-}
+var scoreCount =setInterval(function() {
+	
+	score++;
+	}, 200)
 
 setInterval(function(){
 	if (gameOver == 0)
